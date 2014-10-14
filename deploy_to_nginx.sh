ca3e985fe4d0736cac403a2751dfba1a26a686f3
lockwobr <lockwobr@gmail.com>
2014-10-13 21:59:23 -0500

    change the path to html

--- deploy_to_nginx.sh
+++ deploy_to_nginx.sh
@@ -4,8 +4,8 @@
 
 sudo mv index.html index-old.html 
 
-sudo cp ./index.html /usr/share/nginx/html
-sudo cp ./admin.html  /usr/share/nginx/html
+sudo cp ./app/index.html /usr/share/nginx/html
+sudo cp ./admin/admin.html  /usr/share/nginx/html
 
 sudo cp -R ./build/ /usr/share/nginx/html
 sudo cp -R ./img/ /usr/share/nginx/html
