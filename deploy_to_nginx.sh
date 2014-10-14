9e7cc75fac59cb24ddde5c7a43dbceb2e756d660
lockwobr <lockwobr@gmail.com>
2014-10-13 22:06:13 -0500

    added clean up code

--- deploy_to_nginx.sh
+++ deploy_to_nginx.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
+	
+# clean up
+
+sudo rm -rf build/
+sudo rm -rf img/
+sudo rm -rf index
+sudo rm -rf index.html 
+sudo rm -rf admin.html 
+
+#deploy
 
 sudo cp ./app/index.html /usr/share/nginx/html
 sudo cp ./admin/admin.html  /usr/share/nginx/html
